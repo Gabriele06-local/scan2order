c3e670ccfa524bfc9fe0b0233d4fc745e929aadd
Gabriele06-local <gabrielepau2006@gmail.com>
2026-07-26 18:19:13 +0200

    Replace SVG eye icons with text Mostra/Nascondi for password visibility

--- src/components/svelte/StaffLogin.svelte
+++ src/components/svelte/StaffLogin.svelte
@@ -44,12 +44,8 @@
       <div class="relative">
         <input type={showPw ? 'text' : 'password'} placeholder="Password" bind:value={password} required
           class="w-full border border-gray-200 rounded-xl px-4 py-3 pr-10 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500/20 focus:border-blue-500" />
-        <button type="button" onclick={() => showPw = !showPw} class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600">
-          {#if showPw}
-            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.878 9.878L3 3m6.878 6.878L21 21"/></svg>
-          {:else}
-            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"/></svg>
-          {/if}
+        <button type="button" onclick={() => showPw = !showPw} class="absolute right-2 top-1/2 -translate-y-1/2 text-xs text-blue-600 hover:text-blue-800 font-medium bg-white px-2 py-1 rounded border border-gray-200">
+          {showPw ? 'Nascondi' : 'Mostra'}
         </button>
       </div>
       {#if error}
